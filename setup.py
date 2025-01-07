04a3e4b801ac68073f9d97484c5e2be2abe2e39b
Matt <39347872+Chaaronn@users.noreply.github.com>
2025-01-07 18:18:21 +0000

    update setup

--- setup.py
+++ setup.py
@@ -4,11 +4,19 @@ setup(
     name="Pyolice",
     version="0.1.0",
     description="A Python wrapper for the UK Police API.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Matt Voce",
     author_email="@mozmail.com",
+    url="https://github.com/Chaaronn/Pyolice",
     packages=find_packages(),
     install_requires=[
-        "requests",
+        "requests>=2.25.0",
     ],
     python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
